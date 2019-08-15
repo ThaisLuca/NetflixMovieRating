a2202685c4915a5507a8e868db0a15536e8d557c
thaisluca <thaisluca@uol.com.br>
2019-08-15 19:41:12 -0300

    Adding data viewing method

--- bin/data.py
+++ bin/data.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 from os import listdir
 from os.path import isfile, join
+import matplotlib.pyplot as plt
 
 TRAINING_SET_PATH = '/resource/combined_data/'	
 
@@ -24,10 +25,33 @@ def load_dataset(path):
 
 	print("Loading files...")
 	dt = pd.read_csv(file_path + files[1], header = None, names = ['CustomerID', 'Rating'])
-	for file in files[0:]:
-		dt.append(pd.read_csv(file_path + file,  delimiter = ','))
+	#for file in files[0:]:
+		#dt.append(pd.read_csv(file_path + file,  delimiter = ','))
 
 	dt['Rating'] = dt['Rating'].astype(float)
-	print("%d movies." % dt.size)
-	print("The shape of the dataframe is {}".format(dt.shape))
+	print('Full dataset shape: {}'.format(dt.shape))
 	return dt
+
+# Shows more information about the dataset 
+def get_data_information(data):
+	p = data.groupby('Rating')['Rating'].agg(['count'])
+
+	# Counts the number of movies
+	movie_count = data.isnull().sum()[1]
+
+	# Counts the number of costumers
+	cust_count = data['CustomerID'].nunique() - movie_count
+
+	# Counts the number of ratings
+	rating_count = data['CustomerID'].count() - movie_count
+
+	ax = p.plot(kind='barh', legend=False, figsize=(15,10))
+	plt.title('Total pool: {:,} Movies {:,} Customers, {:,} Ratings given'. format(movie_count, cust_count, rating_count), fontsize=20)
+	plt.axis('off')
+
+	for i in range(1,6):
+		ax.text(p.iloc[i-1][0]/4, i-1, 'Rating {}: {:.0f}%'.format(i, p.iloc[i-1][0]*100 / p.sum()[0]), color='white', weight='bold')
+
+	plt.show()
+
+get_data_information(load_dataset(TRAINING_SET_PATH))
