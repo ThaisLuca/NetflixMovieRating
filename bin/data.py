265dd0b28d621e3be3687710b1942e15162d7fea
thaisluca <thaisluca@uol.com.br>
2019-08-15 19:17:12 -0300

    Adding method to load dataset as a Pandas DataFrame

--- bin/data.py
+++ bin/data.py
@@ -4,16 +4,30 @@
 # Created by Thais Luca
 # PESC/COPPE/UFRJ
 
-import os
+import os, sys
 import pandas as pd
 
-DATASET_SET_PATH = '/resource/NetflixShows.csv'	
+from os import listdir
+from os.path import isfile, join
 
-def load_dataset():
+TRAINING_SET_PATH = '/resource/combined_data/'	
 
-	file_path = os.getcwd() + DATASET_SET_PATH
+# Lists all files in a folder
+def get_list_files(path):
+	return [f for f in listdir(path) if isfile(join(path, f))]
+
+# Loads the csv file into a DataFrame
+def load_dataset(path):
+
+	file_path = os.getcwd() + path
+	files = get_list_files(file_path)[:2]
 
 	print("Loading files...")
-	dt = pd.read_csv(file_path,  delimiter = ',')
+	dt = pd.read_csv(file_path + files[1], header = None, names = ['CustomerID', 'Rating'])
+	for file in files[0:]:
+		dt.append(pd.read_csv(file_path + file,  delimiter = ','))
 
+	dt['Rating'] = dt['Rating'].astype(float)
+	print("%d movies." % dt.size)
+	print("The shape of the dataframe is {}".format(dt.shape))
 	return dt
